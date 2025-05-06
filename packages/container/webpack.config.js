881e5c66034e45d0d630034a085d16ac7980ff5b
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-06 04:47:56 +0200

    initial commit

---
+++ packages/container/webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "swc-loader",
+          options: {
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                jsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: "automatic",
+                },
+              },
+              target: "es2022",
+            },
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
